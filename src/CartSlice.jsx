78aa0e7ace7e575ff7d450b146c60d2861bf7159
yangyang-eng <anh.duong.lrn99@gmail.com>
2024-11-30 15:08:45 -0500

    Updasate Redux

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -1,5 +1,6 @@
 import { createSlice } from '@reduxjs/toolkit';
 
+
 export const CartSlice = createSlice({
   name: 'cart',
   initialState: {
@@ -7,17 +8,32 @@ export const CartSlice = createSlice({
   },
   reducers: {
     addItem: (state, action) => {
-    
+        const { name, image, cost } = action.payload;
+        const existingItem = state.items.find(item.name === name );
+        if (existingItem) {
+            existingItem.quantity++;
+    } else {
+        state.items.push({ name, image, cost, quantity: 1})
+    }
+   
     },
+
+
     removeItem: (state, action) => {
+        state.items = state.items.filter(item => item.name !== action.payload);
     },
     updateQuantity: (state, action) => {
-
-    
+        const { name, quantity} = action.payload;
+        const itemToUpdate = state.item.find(item => ite.name === name);
+        if (itemToUpdate)
+            itemToUpdate.quantity = quantity;
+   
     },
   },
 });
 
+
 export const { addItem, removeItem, updateQuantity } = CartSlice.actions;
 
-export default CartSlice.reducer;
+
+export default CartSlice.reduce
